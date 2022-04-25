978a28c59c8a3dc12e084be0f87ce6657d90f2d2
melis-archabaev <melis99mrx@gmail.com>
2022-04-25 18:23:28 +0600

    Create smtp server with .NET

---
+++ ConsoleNetMail/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace ConsoleNetMail
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential("", "");
+            client.Port = 587;
+            client.Host = "smtp.yandex.ru";
+            client.EnableSsl = true;
+            message.To.Add("melis99mrx@gmail.com");
+            message.From = new MailAddress("");
+            message.IsBodyHtml = true;
+            message.Subject = "Оповещание";
+            message.Body = "Это сообщение от правительство налоговая";
+            client.Send(message);
+            Console.WriteLine("Successfully");
+            client.Dispose();
+        }
+    }
+}
